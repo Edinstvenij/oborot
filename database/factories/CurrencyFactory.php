5d0e94e4001de95c9cce246a9162d79ac80fa9f7
Yura <yuravigovskij@gmail.com>
2023-02-03 14:39:21 +0200

    add Currency migration, model, factory, seed.

---
+++ database/factories/CurrencyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Currency>
+ */
+class CurrencyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'code' => '840',
+            'cipher' => 'USD',
+            'name' => 'Доллар США',
+            'remainder' => 0
+        ];
+    }
+}
---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Currency;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currency = [
+            [
+                'code' => '840',
+                'cipher' => 'USD',
+                'name' => 'Доллар США',
+                'remainder' => 0
+            ],
+            [
+                'code' => '978',
+                'cipher' => 'EUR',
+                'name' => 'Евро',
+                'remainder' => 0
+            ],
+            [
+                'code' => '826',
+                'cipher' => 'GBP',
+                'name' => 'Английский фунт',
+                'remainder' => 0
+            ],
+            [
+                'code' => '036',
+                'cipher' => 'AUD',
+                'name' => 'Австралийский доллар',
+                'remainder' => 0
+            ],
+            [
+                'code' => '985',
+                'cipher' => 'PLN',
+                'name' => 'Польский злотый',
+                'remainder' => 0
+            ],
+            [
+                'code' => '203',
+                'cipher' => 'CZK',
+                'name' => 'Чешская крона',
+                'remainder' => 0
+            ],
+            [
+                'code' => '348',
+                'cipher' => 'HUF',
+                'name' => 'Венгерский форинт',
+                'remainder' => 0
+            ],
+            [
+                'code' => '124',
+                'cipher' => 'CAD',
+                'name' => 'Канадский доллар',
+                'remainder' => 0
+            ],
+            [
+                'code' => '208',
+                'cipher' => 'DKK',
+                'name' => 'Датская крона',
+                'remainder' => 0
+            ],
+            [
+                'code' => '949',
+                'cipher' => 'TRY',
+                'name' => 'Турецкая лира',
+                'remainder' => 0
+            ],
+            [
+                'code' => '756',
+                'cipher' => 'CHF',
+                'name' => 'Швейцарский франк',
+                'remainder' => 0
+            ],
+            [
+                'code' => '752',
+                'cipher' => 'SEK',
+                'name' => 'Шведская крона',
+                'remainder' => 0
+            ],
+            [
+                'code' => '376',
+                'cipher' => 'ILS',
+                'name' => 'Израильский шекель',
+                'remainder' => 0
+            ],
+            [
+                'code' => '946',
+                'cipher' => 'RON',
+                'name' => 'Румынский лей',
+                'remainder' => 0
+            ],
+            [
+                'code' => '498',
+                'cipher' => 'MDL',
+                'name' => 'Молдовский лей',
+                'remainder' => 0
+            ],
+            [
+                'code' => '578',
+                'cipher' => 'NOK',
+                'name' => 'Норвежская крона',
+                'remainder' => 0
+            ],
+            [
+                'code' => '156',
+                'cipher' => 'CNY',
+                'name' => 'Китайский юань',
+                'remainder' => 0
+            ],
+            [
+                'code' => '784',
+                'cipher' => 'AED',
+                'name' => 'Дирхам ОАЭ',
+                'remainder' => 0
+            ],
+            [
+                'code' => '392',
+                'cipher' => 'JPY',
+                'name' => 'Японская йена',
+                'remainder' => 0
+            ],
+            [
+                'code' => '981',
+                'cipher' => 'GEL',
+                'name' => 'Грузинский лари',
+                'remainder' => 0
+            ],
+            [
+                'code' => '944',
+                'cipher' => 'AZN',
+                'name' => 'Азербайджанский манат',
+                'remainder' => 0
+            ],
+            [
+                'code' => '398',
+                'cipher' => 'KZT',
+                'name' => 'Казахстанский тенге',
+                'remainder' => 0
+            ],
+            [
+                'code' => '980',
+                'cipher' => 'UAH',
+                'name' => 'Украинская гривна',
+                'remainder' => 0
+            ],
+        ];
+
+        foreach ($currency as $value) {
+            Currency::factory()->make([
+                'code' => $value['code'],
+                'cipher' => $value['cipher'],
+                'name' => $value['name'],
+                'remainder' => $value['remainder'],
+            ])->save();
+        }
+    }
+}
