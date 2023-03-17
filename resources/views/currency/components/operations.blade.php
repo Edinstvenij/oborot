c3761f2621ac68583badd44b1a1f6f66eac59e6d
Yura <yuravigovskij@gmail.com>
2023-03-17 13:02:18 +0200

    Add scroll to table operations

--- resources/views/currency/components/operations.blade.php
+++ resources/views/currency/components/operations.blade.php
@@ -1,55 +1,61 @@
 @if(count($operations))
     <div class="container">
         <h2 class="h2 text-white">История операций</h2>
-        <table id="currencies" class="table table-dark table-hover ">
-            <thead>
-            <tr>
-                @php($operation = $operations->first())
-                @if($operation->currency_cipher)
-                    <th scope="col">Шифр</th>
-                @endif
-                @if($operation->sum)
-                    <th scope="col">Сумма</th>
-                @endif
-                @if($operation->course)
-                    <th scope="col">Курс операции</th>
-                @endif
-                @if($operation->sum_donor)
-                    <th scope="col">Гривна</th>
-                @endif
-                @if($operation->comment)
-                    <th scope="col">Комментарий</th>
-                @endif
-                @if($operation->date)
-                    <th scope="col">Дата</th>
-                @endif
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($operations as $operation)
-                <tr class="row-link">
+        <div class="scroll-table">
+            <table id="currencies" class="table table-dark table-hover">
+                <thead>
+                <tr>
+                    @php($operation = $operations->first())
                     @if($operation->currency_cipher)
-                        <td>{{ $operation->currency_cipher}}</td>
+                        <th scope="col">Шифр</th>
                     @endif
-                    @if($operation->sum !== null)
-                        <td>{{ $operation->sum}}</td>
+                    @if($operation->sum)
+                        <th scope="col">Сумма</th>
                     @endif
-                    @if($operation->course !== null)
-                        <td>{{ $operation->course}}</td>
+                    @if($operation->course)
+                        <th scope="col">Курс операции</th>
                     @endif
-                    @if($operation->sum_donor !== null)
-                        <td>{{ $operation->sum_donor}}</td>
+                    @if($operation->sum_donor)
+                        <th scope="col">Гривна</th>
                     @endif
                     @if($operation->comment)
-                        <td>{{ $operation->comment}}</td>
+                        <th scope="col">Комментарий</th>
                     @endif
                     @if($operation->date)
-                        <td>{{ $operation->date}}</td>
+                        <th scope="col">Дата</th>
                     @endif
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+            </table>
+            <div class="scroll-table-body">
+                <table id="currencies" class="table table-dark table-hover">
+                    <tbody>
+                    @foreach($operations as $operation)
+                        <tr class="row-link">
+                            @if($operation->currency_cipher)
+                                <td>{{ $operation->currency_cipher}}</td>
+                            @endif
+                            @if($operation->sum !== null)
+                                <td>{{ $operation->sum}}</td>
+                            @endif
+                            @if($operation->course !== null)
+                                <td>{{ $operation->course}}</td>
+                            @endif
+                            @if($operation->sum_donor !== null)
+                                <td>{{ $operation->sum_donor}}</td>
+                            @endif
+                            @if($operation->comment)
+                                <td>{{ $operation->comment}}</td>
+                            @endif
+                            @if($operation->date)
+                                <td>{{ $operation->date}}</td>
+                            @endif
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @else
     <div class="container">
