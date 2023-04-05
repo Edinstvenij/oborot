1cd2308a408254b3debb011cf9c2af2ed1e80f61
Yura <yuravigovskij@gmail.com>
2023-04-05 20:11:43 +0300

    rebuild test Feature.ResponsePageTest.php

--- tests/Feature/ResponsePageTest.php
+++ tests/Feature/ResponsePageTest.php
@@ -11,89 +11,85 @@ class ResponsePageTest extends TestCase
 {
     use RefreshDatabase;
 
+    /** @var bool */
     protected bool $seed = true;
 
-    public function test_indexPage(): void
+    /**
+     * @dataProvider dataProviderForPages
+     * @param string $page
+     * @param int $code
+     * @return void
+     */
+    public function testStatusPage(string $page, int $code): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(302);
+        $this->get($page)->assertStatus($code);
     }
 
-    public function test_currencyPage(): void
+    /**
+     * @return array<int, array<string, string|int>>
+     */
+    public function dataProviderForPages(): array
     {
-        $response = $this->get('/currency');
-
-        $response->assertStatus(200);
+        return [
+            [
+                'page' => '/',
+                'code' => 302,
+            ],
+            [
+                'page' => '/currency',
+                'code' => 200,
+            ],
+        ];
     }
 
-    public function test_currencyHistoryPage(): void
+    /**
+     * @return void
+     */
+    public function testCurrencyHistoryPage(): void
     {
         $date = Carbon::yesterday()->format('Y-m-d');
-        $response = $this->get("/currency?date=$date");
-
-        $response->assertStatus(200);
+        $this->get("/currency?date=$date")
+            ->assertStatus(200);
     }
 
-    public function test_currenciesBuyPages(): void
+    /**
+     * @dataProvider dataProviderForCurrencies
+     * @param string $page
+     * @return void
+     */
+    public function testCurrenciesPage(string $page): void
     {
-        $currencies = Currency::all();
-        foreach ($currencies as $currency) {
-            $response = $this->get("/currency/$currency->cipher/buy");
-            $response->assertStatus(200);
+        foreach (Currency::all() as $currency) {
+            $this->get("/currency/$currency->cipher/$page")
+                ->assertStatus(200);
         }
-        $response = $this->get("/currency/all/buy");
-        $response->assertStatus(200);
-    }
-
-
-    public function test_currenciesSalePages(): void
-    {
-        $currencies = Currency::all();
-        foreach ($currencies as $currency) {
-            $response = $this->get("/currency/$currency->cipher/sale");
-            $response->assertStatus(200);
-        }
-        $response = $this->get("/currency/all/sale");
-        $response->assertStatus(200);
-    }
-
-    public function test_currenciesParishesPages(): void
-    {
-        $currencies = Currency::all();
-        foreach ($currencies as $currency) {
-            $response = $this->get("/currency/$currency->cipher/parishes");
-            $response->assertStatus(200);
-        }
-        $response = $this->get("/currency/all/parishes");
-        $response->assertStatus(200);
-    }
-
-    public function test_currenciesExpensesPages(): void
-    {
-        $currencies = Currency::all();
-        foreach ($currencies as $currency) {
-            $response = $this->get("/currency/$currency->cipher/expenses");
-            $response->assertStatus(200);
-        }
-
-        $response = $this->get("/currency/all/expenses");
-        $response->assertStatus(200);
-    }
-
-    public function test_currenciesShows()
-    {
-        $currencies = Currency::all();
-        foreach ($currencies as $currency) {
-            $response = $this->get("/currency/$currency->cipher");
-            $response->assertStatus(200);
-
-            $response = $this->get("/currency/$currency->cipher/edit");
-            $response->assertStatus(200);
-
-            $response = $this->delete("/currency/$currency->cipher");
-            $response->assertStatus(302);
+        if ($page != 'edit') {
+            $this->get("/currency/all/$page")
+                ->assertStatus(200);
         }
     }
 
+    /**
+     * @return array<int, array<string, string|int>>
+     */
+    public function dataProviderForCurrencies(): array
+    {
+        return [
+            [
+                'buy',
+            ],
+            [
+                'sale',
+            ],
+            [
+                'parishes',
+            ],
+            [
+                'expenses',
+            ],
+            [
+                'edit',
+            ],
+        ];
+    }
 }
